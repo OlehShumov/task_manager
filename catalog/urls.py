0f74d8dc3c6b16723309435be73ca1d7ea6ed95b
OlehShumov <shumovoleh@ukr.net>
2023-11-23 00:16:27 +0200

    fix urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,16 @@ from django.urls import path
 from .views import (index,
                     PositionListView,
                     WorkerListView,
-                    TaskListView)
+                    TaskListView,
+                    PositionCreateView,
+                    PositionUpdateView,
+                    PositionDeleteView,
+                    WorkerCreateView,
+                    WorkerUpdateView,
+                    WorkerDeleteView,
+                    TaskCreateView,
+                    TaskUpdateView,
+                    TaskDeleteView,)
 
 urlpatterns = [
     path("", index, name="index"),
@@ -13,14 +22,59 @@ urlpatterns = [
         name="task-list",
     ),
     path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update",
+    ),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task-create",
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete",
+    ),
+    path(
         "workers/",
         WorkerListView.as_view(),
         name="worker-list",
     ),
     path(
+        "workers/<int:pk>/update/",
+        WorkerUpdateView.as_view(),
+        name="worker-update",
+    ),
+    path(
+        "workers/<int:pk>/delete/",
+        WorkerDeleteView.as_view(),
+        name="worker-delete",
+    ),
+    path(
+        "workers/create/",
+        WorkerCreateView.as_view(),
+        name="worker-create",
+    ),
+    path(
         "positions/",
         PositionListView.as_view(),
         name="position-list",
+    ),
+    path(
+        "positions/<int:pk>/update/",
+        PositionUpdateView.as_view(),
+        name="position-update",
+    ),
+    path(
+        "positions/<int:pk>/delete/",
+        PositionDeleteView.as_view(),
+        name="position-delete",
+    ),
+    path(
+        "positions/create/",
+        PositionCreateView.as_view(),
+        name="position-create",
     ),
 ]
 
