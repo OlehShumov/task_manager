4bbdd2ae1472367d20e07562f7eba5141618b1a6
OlehShumov <shumovoleh@ukr.net>
2023-11-24 00:49:33 +0200

    add tests for models

---
+++ catalog/tests/test_model.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.utils import timezone
+from catalog.models import Task, TaskType, Position, Worker
+
+
+class TaskModelTest(TestCase):
+    def setUp(self):
+        task_type = TaskType.objects.create(name="Test Task Type")
+        position = Position.objects.create(name="Test Position")
+        worker = Worker.objects.create(username="test_user", position=position)
+        deadline = timezone.now().date()
+
+        self.task = Task.objects.create(
+            name="Test Task",
+            description="Test Description",
+            deadline=deadline,
+            is_completed=False,
+            priority=Task.Priority.HIGH,
+            task_type=task_type,
+        )
+        self.task.assignees.add(worker)
+
+    def test_task_str_method(self):
+        self.assertEqual(
+            str(self.task),
+            "Task: Test Task, status: In work, priority: H"
+        )
+
+    def test_task_priority_choices(self):
+        task = Task.objects.create(
+            name="Test Task 2",
+            description="Test Description 2",
+            deadline=timezone.now().date(),
+            is_completed=False,
+            priority=Task.Priority.LOW,
+        )
+        self.assertEqual(task.priority, Task.Priority.LOW)
