b2aae6237e560b6ff37756f0034b6f7f2cd53345
OlehShumov <shumovoleh@ukr.net>
2023-11-23 16:27:16 +0200

    add WorkerCreationForm in forms.py

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Task, Worker
 
 
 class WorkerSearchForm(forms.Form):
@@ -38,3 +42,22 @@ class TaskSearchForm(forms.Form):
             }
         )
     )
+
+
+class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "position",
+        )
